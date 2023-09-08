9a2d636ed479985d0c2fcb9441dcff45cd2db1f0
kk69njit <kk69@njit.edu>
2023-09-07 21:23:18 -0400

    Added customizations to the otherwise simple app

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -2,18 +2,33 @@ package com.example.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatDelegate
+import com.github.matteobattilana.weather.PrecipType
+import com.github.matteobattilana.weather.WeatherView
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // This forces app into light mode.
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+
+        // This is the rain particles
+        val weatherView = findViewById<WeatherView>(R.id.weather_view)
+        weatherView.apply {
+            setWeatherData(PrecipType.RAIN)
+            speed = 1000
+            emissionRate = 90f
+        }
+
+        // This controls the button.
         val button = findViewById<Button>(R.id.helloButton)
         button.setOnClickListener {
-            Log.v("Helloworld", "Button Clicked!")
-            Toast.makeText(this, "Hello to you too!", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "Hello to you too, friend. Stay dry!", Toast.LENGTH_SHORT).show()
         }
     }
 }
